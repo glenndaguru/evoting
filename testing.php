abd2b61a7f240a1e11d399d78129f2da02c035d8
glenndaguru <glenn.masango101@gmail.com>
2017-05-18 12:20:40 +0200

    Insert statement
    Signed-off-by: glenndaguru <glenn.masango101@gmail.com>

--- testing.php
+++ testing.php
@@ -8,7 +8,7 @@
 	$myObj->result = "User succesfully registered";
 	$myJobj = json_encode($myObj);
 	
-	$sql = "INSERT INTO User (userName, userEmail, userPass, userNO) VALUES ('$userName', '$userEmail', '".md5($user_passwordhash)."','$userNo')";
+	$sql = "INSERT INTO User (userName, userEmail, userPass, userNO) VALUES ($userName, $userEmail, '".md5($user_passwordhash)."',$userNo)";
 	if (!mysqli_query($the_connection, $sql)) 
 	{
 		echo "Error: " . $sql . "<br>" . mysqli_error($the_connection);
