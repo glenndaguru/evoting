e678c23366b02c25972c910cda3791c8aafafefa
glenndaguru <glenn.masango101@gmail.com>
2017-05-18 12:40:20 +0200

    connect_error Signed-off-by: glenndaguru <glenn.masango101@gmail.com>

--- testing.php
+++ testing.php
@@ -1,17 +1,17 @@
 <?php
 	include(dbConnection.php);
 	
-	$userName = "Noise";
-	$userEmail = "Noise@gmail.com";
-	$userPass = "Noise";
-	$userNo = "0128004527";
-	$myObj->result = "User succesfully registered";
-	$myJobj = json_encode($myObj);
-	
+	// Connection
 	$servername = "127.4.48.2";
 	$username = "adminD35M7Lk";
 	$password = "M1iBd32D8Hdt";
 	$dbname = "crimereporter";
+	
+	// Variable
+	$userName = "Noise";
+	$userEmail = "Noise@gmail.com";
+	$userPass = "Noise";
+	$userNo = "0128004527";
 
 	// Create connection
 	$conn = new mysqli($servername, $username, $password);
@@ -21,18 +21,20 @@
 	{
 		die("Connection failed: " . $conn->connect_error);
 	} 
-	echo "Connected successfully";
-	/*
-	$sql = "INSERT INTO User (userName, userEmail, userPass, userNO) VALUES ('".$userName."', '".$userEmail."', '".md5($user_passwordhash)."','".$userNo."')";
+	
+	$sql = 'INSERT INTO User'.'(userName, userEmail, userPass, userNO)'.'VALUES ("'.$userName.'", "'.$userEmail.'","'.md5($user_passwordhash).'","'.$userNo.'")';
 	if (!mysqli_query($conn, $sql)) 
 	{
-		echo "Error: " . $sql . "<br>" . mysqli_error($the_connection);
+		echo "Error: " . $sql . "<br>" . mysqli_error($conn);
 	}
 	else
 	{
-		echo $myJobj;
+		$myObj->result = "User succesfully registered";
 	}
-	*/
+	
+	$myJobj = json_encode($myObj);
+	echo $myJobj;
+	
 	mysqli_close($conn);
 	
 ?>
