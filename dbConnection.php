34ea82e3de741eb718b7cfac2c6670369cf980e5
glenndaguru <glenn.masango101@gmail.com>
2017-05-18 10:11:22 +0200

    dbConnection.php added Signed-off-by: glenndaguru <glenn.masango101@gmail.com>

---
+++ dbConnection.php
@@ -0,0 +1,15 @@
+<?php
+	$servername = "127.4.48.2";
+	$username = "adminD35M7Lk";
+	$password = "M1iBd32D8Hdt";
+
+	// Create connection
+	$conn = new mysqli($servername, $username, $password);
+
+	// Check connection
+	if ($conn->connect_error) 
+	{
+		die("Connection failed: " . $conn->connect_error);
+	} 
+	echo "Connected successfully";
+?>
