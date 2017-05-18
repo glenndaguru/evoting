4c91c6542b5faea42879d4ccc3c7120a61d7cee9
glenndaguru <glenn.masango101@gmail.com>
2017-05-18 14:32:22 +0200

    Well Signed-off-by: glenndaguru <glenn.masango101@gmail.com>

--- registration.php
+++ registration.php
@@ -14,13 +14,13 @@
 	} 
 	
 	// Data From App
-	$userName = $_GET['userName'];
-	$userEmail = $_GET['userEmail'];
-	$userPass = $_GET['userPassword'];
-	$userNo = $_GET['userNo'];
+	$userName = $_GET["userName"];
+	$userEmail = $_GET["userEmail"];
+	$userPass = $_GET["userPassword"];
+	$userNo = $_GET["userNo"];
 	
 	// Insert Into User Table
-	$sql = 'INSERT INTO User'.'(userName, userEmail, userPass, userNO)'.'VALUES ("'.$userName.'", "'.$userEmail.'","'.md5($userPass).'","'.$userNo.'")';
+	$sql = 'INSERT INTO User'.'(userName, userEmail, userPass, userNO)'.'VALUES ('.$userName.', '.$userEmail.',"'.md5($userPass).'",'.$userNo.')';
 	if (!mysqli_query($conn, $sql)) 
 	{
 		echo "Error: " . $sql . "<br>" . mysqli_error($conn);
