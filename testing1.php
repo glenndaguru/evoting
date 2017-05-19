5c9d6c938e87db943e9647fe06fc84b411bf080a
glenndaguru <glenn.masango101@gmail.com>
2017-05-19 09:23:25 +0200

    db retreive
    Signed-off-by: glenndaguru <glenn.masango101@gmail.com>

---
+++ testing1.php
@@ -0,0 +1,38 @@
+<?php
+	// Data From App
+	$userEmail = "awe@cs.q";
+	$userPass = "1234";
+	
+	// Connection
+	$servername = "127.4.48.2";
+	$username = "adminD35M7Lk";
+	$password = "M1iBd32D8Hdt";
+	$dbname = "crimereporter";
+	
+	// Create connection
+	$conn = new mysqli($servername, $username, $password, $dbname);
+		
+	if ($conn->connect_error) 
+	{
+		die("Connection failed: " . $conn->connect_error);
+	} 
+	
+	// Selecting User ID, check if user exists
+	$sql = "SELECT userID, FROM User WHERE userEmail = '".$userEmail."' AND userPass ='".$userPass."'";
+	$results = mysqli_query($the_connection,$sql);
+	if(mysqli_num_rows($results) > 0)
+	{
+		$myObj->result = 1;
+	}
+	else
+	{
+		$myObj->result = 0;
+	}
+	
+	$myJobj = json_encode($myObj);
+	echo $myJobj."\n";
+	
+	mysqli_close($conn);
+	
+
+?>
