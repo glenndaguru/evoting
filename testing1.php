fc26b9551fd8204560e4aa8e7e0edf64ad087d52
glenndaguru <glenn.masango101@gmail.com>
2017-05-19 09:37:38 +0200

    sql
    Signed-off-by: glenndaguru <glenn.masango101@gmail.com>

--- testing1.php
+++ testing1.php
@@ -18,7 +18,7 @@
 	} 
 	
 	// Selecting User ID, check if user exists
-	$sql = "SELECT UserID FROM User WHERE userEmail = '".$userEmail."' and userPass = '".$userPass."'";
+	$sql = "SELECT UserID FROM User WHERE userEmail = ".$userEmail." and userPass = ".$userPass."";
 	$result = mysqli_query($sql, $conn) or die("Error in $sql:" . mysqli_error($conn));	
 	if(mysqli_num_rows($results) != 0)
 	{
