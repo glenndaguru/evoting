a662d452cbc328401a78f717abd1010b5a783fc0
glenndaguru <glenn.masango101@gmail.com>
2017-05-19 10:56:54 +0200

    report script
    Signed-off-by: glenndaguru <glenn.masango101@gmail.com>

--- report.php
+++ report.php
@@ -6,19 +6,19 @@
 	$dbname = "crimereporter";
 	
 	// Data From App
-	$userEmail = $_POST(["userEmail"];
-	$userPass = $_POST(["userPassword"];
-	$userNo = $_POST(["userNo"];
-	$userLati= $_POST(["lati"];
-	$userLongi= $_POST(["longi"];
-	$crimeType= $_POST(["type"];
-	$crimeDesc= $_POST(["desc"];
+	$userEmail = "awe@cs.q";//$_POST["userEmail"];
+	$userPass = "1234";//$_POST["userPassword"];
+	$userNo = "0128004527";//$_POST["userNo"];
+	$userLati= $_POST["lati"];
+	$userLongi= $_POST["longi"];
+	$crimeType= $_POST["type"];
+	$crimeDesc= $_POST["desc"];
 	$crimeImg= $_POST["img"];
 	$ansa1 = $_POST["ansa1"];
 	$ansa2 = $_POST["ansa2"];
 	$ansa3 = $_POST["ansa3"];
 	$ansa4 = $_POST["ansa4"];
-	$userID = "";
+	global $userID;
 	
 	// Create connection
 	$conn = new mysqli($servername, $username, $password, $dbname);
@@ -29,15 +29,18 @@
 	} 
 	
 	//Fetch User ID
-	$mysql = "SELECT userID, FROM User WHERE userEmail = '".$userEmail."' and userPass = '".md5($userPass)."'";
-	$result = mysqli_query($the_connection,$mysql);
-	while($row = mysqli_fetch_object($result)) 
+	$mysql = "SELECT userID, FROM User WHERE userEmail = '".$userEmail."' and userPass = '"$userPass."'";
+	$result = mysqli_query($conn,$mysql);
+	if (mysqli_num_rows($result) > 0) 
 	{
-	    $userID = $row->userID;	  
+		while($row =  mysqli_fetch_assoc($result)) 
+		{
+			$GLOBALS['userID'] = $row["userID"];	  
+		}
 	}
 	
 	//Insert Into Report Table
-	$sql = "INSERT INTO User_Report (userID, userLati, userLongi, crimeType, crimeDesc, crimeImg, userAns1, userAns2, userAns3, userAns4) VALUES ('".$userID."', '".$userLati."','".$userLongi."','".$crimeType."','".$crimeDesc."','".$crimeImg."','".$ansa1."','".$ansa2."','".$ansa3."','".$ansa4."')";
+	$sql = "INSERT INTO User_Report (userID, userLati, userLongi, crimeType, crimeDesc, crimeImg, userAns1, userAns2, userAns3, userAns4) VALUES ('".$GLOBALS['userID']."', '".$userLati."','".$userLongi."','".$crimeType."','".$crimeDesc."','".$crimeImg."','".$ansa1."','".$ansa2."','".$ansa3."','".$ansa4."')";
 	if (!mysqli_query($the_connection, $sql)) 
 	{
 		echo "Error: " . $sql . "<br>" . mysqli_error($the_connection);
