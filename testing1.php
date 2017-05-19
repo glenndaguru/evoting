7d5eb712fd79bc04d40cd3e099cf119e32563d8d
glenndaguru <glenn.masango101@gmail.com>
2017-05-19 09:49:14 +0200

    sql Signed-off-by: glenndaguru <glenn.masango101@gmail.com>

--- testing1.php
+++ testing1.php
@@ -15,12 +15,16 @@
 	if ($conn->connect_error) 
 	{
 		die("Connection failed: " . $conn->connect_error);
-	} 
+	}
+	else
+	{
+		echo "Connected";
+	}
 	
 	// Selecting User ID, check if user exists
-	$sql = "SELECT UserID FROM User";
 	//$sql = "SELECT UserID FROM User WHERE userEmail='".$userEmail."' AND userPass='".$userPass."'";
-	$result = mysqli_query($sql, $conn) or die("Error in $sql:" . mysqli_error($conn));	
+	$sql = "SELECT UserID FROM User";
+	$result = mysqli_query($conn,$sql) or die("Error in $sql:" . mysqli_error($conn));	
 	if(mysqli_num_rows($results) != 0)
 	{
 		$myObj->result = 1;
