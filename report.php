1c7aa596271c1715c54b9b609193858492296e17
glenndaguru <glenn.masango101@gmail.com>
2017-05-19 11:40:30 +0200

    awe
    Signed-off-by: glenndaguru <glenn.masango101@gmail.com>

--- report.php
+++ report.php
@@ -33,12 +33,9 @@
 	//Fetch User ID
 	$sql = "SELECT UserID FROM User WHERE userEmail='".$userEmail."' AND userPass='".$userPass."'";
 	$result = mysqli_query($conn,$sql) or die("Error in $sql:" . mysqli_error($conn));
-	if (mysqli_num_rows($result) > 0) 
+	if ($row = mysqli_fetch_object($result)) 
 	{
-		while($row =  mysqli_fetch_assoc($result)) 
-		{
-			$GLOBALS['userID'] = $row["userID"];	  
-		}
+		$GLOBALS['userID'] = $row->userID;	  
 	}
 	
 	//Insert Into Report Table
