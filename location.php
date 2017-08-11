71b59dc41cec3cde001a822db86f9b63816b86af
origin <glenn.masango101@gmail.com>
2017-08-11 14:40:12 +0200

    list locations

---
+++ location.php
@@ -0,0 +1,36 @@
+<?php
+	// Data From App
+	$userLati= $_POST["lati"];
+	$userLongi= $_POST["longi"];
+	
+	// Connection
+	$servername = "127.4.48.2";
+	$username = "adminD35M7Lk";
+	$password = "M1iBd32D8Hdt";
+	$dbname = "crimereporter";
+	
+	// Create connection
+	$conn = new mysqli($servername, $username, $password, $dbname);
+		
+	if ($conn->connect_error) 
+	{
+		die("Connection failed: " . $conn->connect_error);
+	}
+
+	
+	// List all locations
+	$rows = array();
+	$sql = "SELECT * FROM User_Panic";
+	$result = mysqli_query($conn,$sql) or die("Error in $sql:" . mysqli_error($conn));	
+	
+	while($row = mysqli_fetch_assoc($result)) 
+	{
+		$rows[] = $row;
+	}
+	$myJobj = json_encode($myObj);
+	echo $myJobj."\n";
+	
+	mysqli_close($conn);
+	
+
+?>
