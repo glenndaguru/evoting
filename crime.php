189ff62b8e602acbde844f03f32dbadee3afc57d
origin <glenn.masango101@gmail.com>
2017-08-22 15:38:11 +0200

    files added

---
+++ crime.php
@@ -0,0 +1,41 @@
+<?php
+	// Data From App
+	$crimeID = $_POST["crimeID"];
+	
+	// Connection
+	$servername = "127.4.48.2";
+	$username = "adminD35M7Lk";
+	$password = "M1iBd32D8Hdt";
+	$dbname = "crimereporter";
+	
+	// Create connection
+	$conn = new mysqli($servername, $username, $password, $dbname);
+		
+	if ($conn->connect_error) 
+	{
+		die("Connection failed: " . $conn->connect_error);
+	}
+	else
+	{
+		echo "Connected";
+	}
+	
+	
+	// Select all crimes 
+	$rows = array();
+	$sql = "SELECT * FROM User_Report WHERE rep_no ='".$crimeID."'";
+	$result = mysqli_query($conn,$sql) or die("Error in $sql:" . mysqli_error($conn));	
+	
+	while($row = mysqli_fetch_assoc($result)) 
+	{
+		$rows[] = $row;
+	}
+	
+	$myJobj = json_encode(array('Crimes'=> $rows));
+	echo $myJobj."\n";
+	
+	
+	mysqli_close($conn);
+	
+
+?>
